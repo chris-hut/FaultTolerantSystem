c2b67cee888a00feb9771cd3ad23bc6d15513d72
Chris Hut <tophernuts@gmail.com>
2014-10-19 06:27:58 -0600

    Added shitty little test suite in InsertionSort

--- src/com/hut/InsertionSort.java
+++ src/com/hut/InsertionSort.java
@@ -1,5 +1,7 @@
 package com.hut;
 
+import java.util.Random;
+
 public class InsertionSort extends Sort {
 
     static{
@@ -7,9 +9,7 @@ public class InsertionSort extends Sort {
     }
 
     public static void main(String[] args){
-        InsertionSort a = new InsertionSort();
-        int[] arr = {3, 2, 1};
-        a.sort(arr);
+        testInsertionSort();
     }
 
     /***
@@ -24,4 +24,51 @@ public class InsertionSort extends Sort {
      }
 
     private native int cSort(int[] values);
+
+    /**
+     * My shitty little test suite since I can't figure out how to get use
+     * JNI with JUnit*/
+    private static void testInsertionSort(){
+        Random rand = new Random();
+        int[] a = {0};
+        InsertionSort is = new InsertionSort();
+        is.sort(a);
+        checkSorted(a);
+        int[] b= {};
+        is.sort(b);
+        checkSorted(b);
+        int[] c= {1,1,1,1,1};
+        is.sort(c);
+        checkSorted(c);
+        int[] d = {4,8,15,16,23,42};
+        is.sort(d);
+        checkSorted(d);
+        int[] e = {108,42,23,16,15,8,4};
+        is.sort(e);
+        checkSorted(e);
+        int[] f = new int[100];
+        for(int i = 0; i < 100; i++){
+            f[i] = rand.nextInt();
+        }
+        is.sort(f);
+        checkSorted(f);
+        int[] g = new int[101];
+        for(int i = 0; i < 100; i++){
+            g[i] = rand.nextInt();
+        }
+        is.sort(g);
+        checkSorted(g);
+
+    }
+
+    private static void checkSorted(int[] a){
+        if(a.length == 0) return;
+        int prev = a[0];
+        for(int i = 1; i < a.length-1; i++){
+            if(prev > a[i]){
+                System.out.println(String.format("A[%d]: %d, A[%d]: %d", i, a[i], i - 1, a[i - 1]));
+            }
+            prev = a[i];
+        }
+    }
 }
--- src/test/InsertionSortTest.java
+++ src/test/InsertionSortTest.java
@@ -1,22 +1,114 @@
 package test;
 
 import com.hut.InsertionSort;
+import com.hut.Sort;
 import junit.framework.TestCase;
 import org.junit.Before;
+
+import java.util.Random;
 
 /**
  * Tests the Java Native method insertion sort
  */
 public class InsertionSortTest extends TestCase {
+    private Sort sort;
 
-    InsertionSort s = null;
+    // TODO: Figure out how to run these or don't
 
-    @Before
+   /* @Before
     public void setUp(){
-        //s = new InsertionSort();
+        sort = new InsertionSort();
     }
 
-    public void testLoadLibrary(){
+    public void testSortOneValue(){
+        int[] a = {0};
+        sort.sort(a);
 
+        // XXX: Should I even be checking the length?
+        assertEquals(a.length, 1);
+        assertEquals(a[0], 0);
     }
+
+    public void testSortArrayEmpty() {
+        int[] a = {};
+        sort.sort(a);
+
+        assertEquals(a.length, 0);
+    }
+
+    public void testSortAllSameValue(){
+        int[] a = {1,1,1,1,1};
+        sort.sort(a);
+
+        assertEquals(a.length, 5);
+        for(int i: a){
+            assertEquals(i, 1);
+        }
+        checkSorted(a);
+    }
+
+    public void testAlreadySorted(){
+        int a[] = {4, 8, 15, 16, 23, 42};
+        sort.sort(a);
+
+        checkSorted(a);
+    }
+
+    public void testReverseSorted(){
+        int a[] = {108, 42, 23, 16, 15, 8, 4};
+        sort.sort(a);
+
+        checkSorted(a);
+    }
+
+    public void testEvenNumberRandomElements(){
+        Random r = new Random();
+        int a[] = new int[100];
+        for(int i = 0; i < 100; i++){
+            a[i] = r.nextInt();
+        }
+
+        sort.sort(a);
+
+        checkSorted(a);
+    }
+
+    public void testOddNumberRandomElements(){
+        Random r = new Random();
+        int a[] = new int[101];
+        for(int i = 0; i < 101; i++){
+            a[i] = r.nextInt();
+        }
+
+        sort.sort(a);
+
+        checkSorted(a);
+    }
+
+    public void testMiddleValuesEqual(){
+        int a[] = {4, 8, 15, 15, 23, 42};
+        sort.sort(a);
+
+        checkSorted(a);
+    }
+
+    private void checkSorted(int[] a){
+        int prev = a[0];
+        for(int i = 1; i < a.length-1; i++){
+            if(prev > a[i]){
+                assertTrue(String.format("A[%d]: %d, A[%d]: %d", i, a[i], i-1, a[i-1]), prev <= a[i]);
+            }
+            prev = a[i];
+        }
+    }
+
+    private void debugPrint(int[]a){
+        StringBuilder sb = new StringBuilder();
+        if(a.length == 0) System.out.println("Array was 0 length");
+        for(int i : a){
+            sb.append(i).append(" ");
+        }
+
+        System.out.println(sb.toString());
+    }*/
 }
