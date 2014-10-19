ecd3aec46159a64873b9678146699944ce1d77ea
Chris Hut <tophernuts@gmail.com>
2014-10-19 16:49:37 -0600

    Removed some junk

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 cd src/com/hut
-rm *.so *.class
+rm *.class
 javac *.java
 cd ../../
 # Generate new Data file
@@ -9,9 +9,6 @@ java com.hut.DataGenerator outfile.txt 10
 : ${JAVA_HOME:?"Need to set JAVA_HOME, on lab machines this is /usr/lib/jvm/java-1.7.0"}
 rm *.so
 javah -jni com.hut.InsertionSort
-cp com_hut_InsertionSort.h com/hut/
 gcc -o libinsertionsort.so -lc -shared -I $JAVA_HOME/include -I $JAVA_HOME/include/linux -fPIC com/hut/insertion_sort.c
-chmod +x libinsertionsort.so
-cp libinsertionsort.so com/hut/
-export LD_LIBRARY_PATH=.:./com/hut/
+export LD_LIBRARY_PATH=.
 java com.hut.Driver outfile.txt sorted.txt 1 1 100
